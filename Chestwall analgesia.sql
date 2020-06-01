59033dad6bbd806cd5d49e683c90cfb53b0fbea6
Peter Jones <peter.jones-2@manchester.ac.uk>
2020-06-01 16:04:30 +0100

    Modified Patient inclusion into select table, changed date of inclusion, added breath & airway supp&status (pre hosp and hosp),
    need to add in 2nd hospital distinction,
    need to add intub/vent 1|0 column

--- Chestwall analgesia.sql
+++ Chestwall analgesia.sql
@@ -8,105 +8,143 @@ mortality in patients where analgesia and strong/regional analgesia is delayed, 
 (or expected mortality vs time till analgesia)
 */ 
 
-/*
---Patient inclusion
-select count(distinct caseid) PtCount 
+
+--Patient inclusion & select table
+If object_ID('tempdb..#select') is not null
+drop table #select
+select --count(distinct caseid) PtCount 
+distinct p.submissionid
+into #select
 from pricache p
 inner join (select AISCode, submissionid, severity, supplementarycode
 			from SubmissionCodingView
 			where aiscode between '450201' and '450214'
 			or aiscode = '450804'
-			--or aiscode between '750900' and '750972'
+			or aiscode between '750900' and '750972'
 			) scv
 			on scv.submissionid = p.SubmissionID
 
 where countryid in (1,2)
-and arvd between '20170101' and '20200228'
 and dispatchDate < '20200529'
+and arvd between '20170301' and '20200228'
 and knownoutcome =1
-*/
 
+--*********************************************************
+--************Air and Breathing supp/status****************
+--*********************************************************
 
+If object_ID('tempdb..#Breathsupp') is not null
+drop table ##Breathsupp
+select SubmissionID, description, case when loc in ('At Scene', 'Enroute') then 'Pre-Hospital' else loc end Loc
+into #breathsupp
+from submissionsectionextview
+inner join (select * from lookup 
+			where lookuptypeid = 'BreathingSupport'
+		) lk on lk.LookupName = AnswerText
+where questionid ='INTER_BSUPP_VAL'
+and submissionid in (select submissionid from #select)
+
+If object_ID('tempdb..#Airsupp') is not null
+drop table #Airsupp
+select SubmissionID, description, case when loc in ('At Scene', 'Enroute') then 'Pre-Hospital' else loc end Loc
+into #airsupp
+from submissionsectionextview
+inner join (select * from lookup 
+			where lookuptypeid = 'AirwaySupport'
+		) lk on lk.LookupName = AnswerText
+where questionid ='INTER_AIRWAYSUPP'
+and submissionid in (select submissionid from #select)
+
+If object_ID('tempdb..#Airstatus') is not null
+drop table #Airstatus
+select SubmissionID, description, case when loc in ('At Scene', 'Enroute') then 'Pre-Hospital' else loc end Loc
+into #airstatus
+from submissionsectionextview
+inner join (select * from lookup 
+			where lookuptypeid = 'AirwayStatus'
+		) lk on lk.LookupName = AnswerText
+where questionid ='ASSESS_AIRWAYS_VAL'
+and submissionid in (select submissionid from #select)
+
+If object_ID('tempdb..#Breathstatus') is not null
+drop table #breathstatus
+select SubmissionID, description, case when loc in ('At Scene', 'Enroute') then 'Pre-Hospital' else loc end Loc
+into #Breathstatus
+from submissionsectionextview
+inner join (select * from lookup
+			where lookuptypeid = 'BreathingStatus'
+		) lk on lk.LookupName = AnswerText
+where questionid ='ASSESS_BREATHS_VAL'
+and submissionid in (select submissionid from #select)
+
+--*********************************************************
+--***********************Main Table************************
+--*********************************************************
+
+If object_ID('tempdb..#ribsdataset') is not null
 drop table #ribsdataset
 select 
-p.SubmissionID,caseID,countryid, dispatchdate,
+p.SubmissionID,caseID,countryid, dispatchdate, crank,
 knownoutcome, mtc,arvdt,age,sex,
 case when sex = 'male' then 1 else 0 end as Male,
-charl,ISS,issband,GCS,intubvent,msev,head,face,
-thor,abdo,spine,pelv,limb,other otherinj, 
-Ps14, psONS,died,los,loscc,mech,mechtype,ttype,
-transfertype,AISCode,SupplementaryCode,severity,
-Injuries,OperDesc operation, OpDT,
+charl,ISS,issband,GCS,intubvent,Ps14, psONS,died,los,loscc,mech,mechtype,
+ttype, transfertype, Injuries,OperDesc operation, OpDT,
+HospBreathSupp, HospAirwaySupp, HospBreathStatus, HospAirwayStatus,
+PrehospBreathSupp,PrehospAirwaySupp, PrehospBreathStatus, prehospAirwayStatus, 
 case	when OperDesc = 'Rib fracture fixation' then 1	else 0	end as RibFixation,
 AnalgesiaLoc,
 isnull (CAST([ANLG DateTime] AS datetime), '2100-01-01 00:01:00.000') AnlgDT,
 Analgesia,
-case 
-	when Analgesia is null and EpiduralAnaesthetic is null then 1 
+case when Analgesia is null and EpiduralAnaesthetic is null then 1 
 	else 0
  end NoAnalgesia,
-case 
-	when EpiduralAnaesthetic is not null then 1
+case when EpiduralAnaesthetic is not null then 1
 	else 0
 	end as EpiduralAnaesthetic,
-case
-	when Analgesia is null and EpiduralAnaesthetic is not null then 'Epidural'
+case when Analgesia is null and EpiduralAnaesthetic is not null then 'Epidural'
 	when Analgesia = 'other' and EpiduralAnaesthetic is not null then 'Epidural'
 	when Analgesia is null and EpiduralAnaesthetic is null then 'N/a'
 	else Analgesia
 	end as CombAnalgesia,
-case
-	when Analgesia is null and EpiduralAnaesthetic is null then 1
+case when Analgesia is null and EpiduralAnaesthetic is null then 1
 	else 0
 	end as [N/a],
-case
-	when analgesia = 'Intravenous opioid' then 1
+case when analgesia = 'Intravenous opioid' then 1
 	else 0
 	end as Intravenousopioid,
-case
-	when  analgesia = 'Intravenous paracetamol' then 1
+case when  analgesia = 'Intravenous paracetamol' then 1
 	else 0
 	end as IntravenousParacetamol,
-case
-	when analgesia = 'Entonox' then 1
+case when analgesia = 'Entonox' then 1
 	else 0
 	end as Entonox,
-case 
-	when analgesia = 'Ketamine' then 1
+case when analgesia = 'Ketamine' then 1
 	else 0
 	end as Ketamine,
-case
-	when Analgesia = 'Epidural block' then 1
+case when Analgesia = 'Epidural block' then 1
 	when Analgesia = 'other' and EpiduralAnaesthetic is not null then 1
 	when Analgesia is null and EpiduralAnaesthetic is not null then 1
 	else 0
 	end as Epidural,
-case
-	when Analgesia in ('Other','Patient controlled Intravenous Opioid (PCA)',
+case when Analgesia in ('Other','Patient controlled Intravenous Opioid (PCA)',
 						'Local anaesthetic blockade (non epidural/paravertebral)',
-						'Paravertebral block','Local anaesthetic patches','Methoxyflurane') then 1
-	else 0
+						'Paravertebral block','Local anaesthetic patches','Methoxyflurane') 
+	then 1 else 0
 	end as Other,
 case when supplementarycode in ('62','72','82') then 1 else 0
 	end UnknownNoRibFractures,
 RibFractures,
-case
-	when aiscode between '450209' and '450214' then 1
+case when aiscode between '450209' and '450214' then 1
 	else 0
 	end as FlailChest,
-case
-	when aiscode = '450804' then 1
+case when aiscode = '450804' then 1
 	else 0
 	end as SternalFracture
---case
---	when aiscode between '750900' and '750972' then 1
---	else 0 
---	end as ScapulaFracture 
 
 into #ribsdataset
 from PRIcache P
 
-inner join (select AISCode, submissionid, severity, supplementarycode,
+left join (select AISCode, submissionid, severity, supplementarycode,
 			sum(case 
 					when supplementarycode in ('57','59') or aiscode = '450201' then 1
 					when supplementarycode in  ('58','60','61') or aiscode = '450202' then 2
@@ -134,7 +172,7 @@ left join (select submissionID, QuestionID, Description EpiduralAnaesthetic from
 			and Description = 'Epidural Anaesthetic') Epi
 			on epi.submissionID = p.submissionID
 
---***************Maydul's ChestWall Report code ************
+---------------------------
 LEFT JOIN (SELECT SubmissionID AngID, CAST([AGDate] + ' ' + LEFT(AGTime,2) + ':' + RIGHT(AGTime, 2) AS DATETIME) AS [ANLG DateTime], 
 			ROW_NUMBER () OVER (PARTITION BY SUBMISSIONID ORDER BY CAST([AGDate] + ' ' + LEFT(AGTime,2) + ':' + RIGHT(AGTime, 2) AS DATETIME)) ANRNK,
 			Description Analgesia, loc AnalgesiaLoc
@@ -163,7 +201,7 @@ LEFT JOIN (SELECT SubmissionID AngID, CAST([AGDate] + ' ' + LEFT(AGTime,2) + ':'
 		WHERE  S.QuestionID = 'INTER_ANAL_ANALG' AND LookupTypeID = 'AnalgesiaName' 
 		AND CAST([AGDate] + ' ' + LEFT(AGTime,2) + ':' + RIGHT(AGTime, 2) AS DATETIME) IS NOT NULL
 ) ANG ON P.SubmissionID = ANG.AngID 
-
+----------------------------
 
 LEFT JOIN (SELECT S.SubmissionID OpID, S.SubmissionSectionID SSOpID, DESCRIPTION OperDesc, OpDT
 			FROM SubmissionSectionView S
@@ -183,15 +221,83 @@ LEFT JOIN (SELECT S.SubmissionID OpID, S.SubmissionSectionID SSOpID, DESCRIPTION
 					)Lk ON ANSWERTEXT = LookupName 
 			WHERE  S.QuestionID = 'INTER_PROC_PROC' AND Description = 'RIB FRACTURE FIXATION'
 	) OP ON P.SubmissionID = OP.OpID
-	 
-	
---*****************************************
-where countryid in (1,2)
-and arvd between '20170101' and '20200228'
-and dispatchDate < '20200529'
-and knownoutcome =1
 
+---------------------------
 
+left join(	select distinct submissionid, 
+			stuff((select ', ' + t.[description]
+       				from #breathsupp t
+       				where t.SubmissionID = #breathsupp.SubmissionID
+					and loc = 'Pre-Hospital'
+					order by t.[Description]
+       				for xml path('')
+ 				),1,1,'') as PrehospBreathSupp,
+			stuff((select ', ' + t.[description]
+       				from #breathsupp t
+       				where t.SubmissionID = #breathsupp.SubmissionID
+					and loc != 'Pre-Hospital'
+					order by t.[Description]
+       				for xml path('')
+ 				),1,1,'') as HospBreathSupp
+			from #breathsupp
+	) Bsu on Bsu.SubmissionID = p.SubmissionID
+left join(	select distinct submissionid, 
+			stuff((select ', ' + t.[description]
+       				from #AirSupp t
+       				where t.SubmissionID = #AirSupp.SubmissionID
+					and loc = 'Pre-Hospital'
+					order by t.[Description]
+       				for xml path('')
+ 				),1,1,'') as PrehospAirwaySupp,
+			stuff((select ', ' + t.[description]
+       				from #AirSupp t
+       				where t.SubmissionID = #AirSupp.SubmissionID
+					and loc != 'Pre-Hospital'
+					order by t.[Description]
+       				for xml path('')
+ 				),1,1,'') as HospAirwaySupp
+			 from #airsupp
+		)AwSu on AwSu.SubmissionID = p.SubmissionID
+left join(	select distinct submissionid, 
+			stuff((select ', ' + t.[description]
+       				from #AirStatus t
+       				where t.SubmissionID = #AirStatus.SubmissionID
+					and loc = 'Pre-Hospital'
+					order by t.[Description]
+       				for xml path('')
+ 				),1,1,'') as PrehospAirwayStatus,
+			stuff((select ', ' + t.[description]
+       				from #AirStatus t
+       				where t.SubmissionID = #AirStatus.SubmissionID
+					and loc != 'Pre-Hospital'
+					order by t.[Description]
+       				for xml path('')
+ 				),1,1,'') as HospAirwayStatus 
+			from #airstatus
+		)AwSt on AwSt.SubmissionID = p.SubmissionID
+left join(	select distinct submissionid, 
+			stuff((select ', ' + t.[description]
+       				from #BreathStatus t
+       				where t.SubmissionID = #BreathStatus.SubmissionID
+					and loc = 'Pre-Hospital'
+					order by t.[Description]
+       				for xml path('')
+ 				),1,1,'') as PrehospBreathStatus,
+			stuff((select ', ' + t.[description]
+       				from #BreathStatus t
+       				where t.SubmissionID = #BreathStatus.SubmissionID
+					and loc != 'Pre-Hospital'
+					order by t.[Description]
+       				for xml path('')
+ 				),1,1,'') as HospBreathStatus
+			from #Breathstatus
+		) BSt on BSt.SubmissionID = p.SubmissionID
+
+where p.submissionid in (select submissionid from #select)
+
+--******************************************
+--******************************************
+--******************************************
 
 --select count(distinct caseid)
 --from #ribsdataset
@@ -207,7 +313,7 @@ select  count(distinct caseid)
 --******************************************
 --*****************Analgesia****************
 --******************************************
-
+If object_ID('tempdb..#Analgesia') is not null
 Drop table #Analgesia
 select distinct
 caseID,
@@ -246,12 +352,9 @@ Select
 (Round(100*cast(sum(Other) as float)/count(caseID),2)) [Other%]
 from #Analgesia
 
-/**************************************************************
-********************Aggregation Tables**************************
-**************************************************************/
-
---need to add median LOS
---median age, ISS, proportion with injuries, rib fractures listed, mortality, LOS median,  male
+/*********************************************************
+********************AISCODEs for Sev**********************
+*********************************************************/
 
 If object_id('tempdb..#AISbody') is not null
 drop table #AISBody
@@ -266,13 +369,18 @@ into #AISbody
 from (		select distinct submissionid, bodyarea, severity,
 			dense_rank() over (partition by submissionid, bodyarea order by severity desc) AISAreaRnk --Ranks all injuries within a body region,
 			from submissioncodingview
-			where submissionid in (select submissionid from #ribsdataset)
+			where submissionid in (select submissionid from #select)
 	) A
 where AISAreaRnk =1
 group by submissionid
 
 
+/**************************************************************
+********************Aggregation Table**************************
+**************************************************************/
 
+
+If object_ID('tempdb..#ribsagg') is not null
 drop table #ribsagg
 select 
 #ribsdataset.caseID,
@@ -309,22 +417,32 @@ max(IntravenousParacetamol) IntravenousParacetamol,
 max(Entonox) Entonox,
 max(Ketamine) Ketamine,
 max(Epidural) Epidural,
-max(Other) other
+max(Other) other,
+max(PHBS) PrehospBreathSupp, max(PHAS)PrehospAirwaySupp,
+max(PHBST) PrehospBreathStatus,max(PHAST) PrehospAirwayStatus,
+max(HospBreathSupp) HospBreathSupp, max(HospAirwaySupp) HospAirwaySupp,
+max(HospBreathStatus) HospBreathStatus, max(HospAirwayStatus) HospAirwayStatus
 
 into #ribsagg
 from #ribsdataset
 left join #AISbody on AISid = SubmissionID
-left join (select caseid, 
+left join(	select caseid, 
 			datediff(minute, min(arvdt) , min(AnlgDT)) TimetoAnalgesia,
 			datediff(minute, min(arvdt), min(opdt))TimetoRibFixation
 			from #ribsdataset
 			group by caseid
 		) Timeto on Timeto.caseid = #ribsdataset.caseid
+left join(	select submissionid, PrehospBreathSupp PHBS,PrehospAirwaySupp PHAS, PrehospBreathStatus PHBSt, prehospAirwayStatus PHASt
+			from #ribsdataset
+			where crank =1 
+		)  PH on Ph.SubmissionID = #ribsdataset.SubmissionID
 group by #ribsdataset.caseid
 
 select * from #ribsagg
 
-
+/************************************************
+*****************Analysis tables*****************
+************************************************/
 
 --combined analgesia
 select
@@ -351,9 +469,13 @@ sum(ribfixation) ribfixation
 --sum(scapulafracture) Scapula
 from #ribsagg
 
+
+-----------------------------------------------------------
+------------------Rib Fixation LOS-------------------------
+-----------------------------------------------------------
 --LOS Median
 select 
-RIBfixation,
+Ribfixation,
 count(*)n,
 case
 	when elig % 2 = 1 then mediodd
@@ -376,7 +498,7 @@ left join (select medID, count(*)elig,
 group by RIBfixation, elig, mediodd, medieven, iqrupr, iqrlwr
 order by 1 asc
 
---LOSCC median select 
+--LOSCC median 
 select
 Ribfixation,
 count(*)n,
@@ -399,11 +521,62 @@ left join (select medID, count(*)elig,
 			group by medID) LOSCCmedian
 			on medid = RIBfixation
 where LOscc >0
-group by RIBfixation,elig, mediodd, medieven, iqrupr, iqrlwr
+group by Ribfixation,elig, mediodd, medieven, iqrupr, iqrlwr
 order by 1 asc
 
---*********************************************************
---*********************************************************
---*********************************************************
+-----------------------------------------------------------
+---------------------Flail LOS-----------------------------
+-----------------------------------------------------------
+--LOS Median
+select 
+Flail,
+count(*)n,
+case
+	when elig % 2 = 1 then mediodd
+	else (mediodd+medieven)/2
+	end mediLOS,
+iqrLwr,
+iqrUpr
+from #ribsagg
+left join (select medID, count(*)elig,
+					  max(case when hemi = 2 then val else null end)mediodd,
+					  min(case when hemi = 3 then val else null end)medieven,
+					  min(case when hemi = 2 then val else null end)iqrLwr,
+					  max(case when hemi = 3 then val else null end)iqrUpr
+			   from (select Flail medID, los val,
+							ntile(4) over(partition by Flail order by los)hemi
+					 from #ribsagg
+					 where LOS is not null)x
+			group by medID) LOSmedian
+			on medid = Flail
+group by Flail, elig, mediodd, medieven, iqrupr, iqrlwr
+order by 1 asc
 
-
+--LOSCC median  
+select
+Flail,
+count(*)n,
+case
+	when elig % 2 = 1 then mediodd
+	else (mediodd+medieven)/2
+	end mediLOSCC,
+iqrLwr,
+iqrUpr
+from #ribsagg
+left join (select medID, count(*)elig,
+					  max(case when hemi = 2 then val else null end)mediodd,
+					  min(case when hemi = 3 then val else null end)medieven,
+					  min(case when hemi = 2 then val else null end)iqrLwr,
+					  max(case when hemi = 3 then val else null end)iqrUpr
+			   from (select Flail medID, LOScc val,
+							ntile(4) over(partition by Flail order by losCC)hemi
+					 from #ribsagg
+					 where LOScc >0)x
+			group by medID) LOSCCmedian
+			on medid = Flail
+where LOscc >0
+group by Flail,elig, mediodd, medieven, iqrupr, iqrlwr
+order by 1 asc
+--*********************************************************
+--*********************************************************
+--*********************************************************
